9c2382bc53708b14ce7c265dde2d8bbde1c1ae6c
Fatih Kadir Akın <fatihkadirakin@gmail.com>
2026-01-08 11:48:43 +0300

    refactor(pages): update _error.tsx to use type annotations

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,11 +1,12 @@
 import * as Sentry from "@sentry/nextjs";
-import Error from "next/error";
+import type { NextPageContext } from "next";
+import Error, { ErrorProps } from "next/error";
 
-const CustomErrorComponent = (props) => {
+const CustomErrorComponent = (props: ErrorProps) => {
   return <Error statusCode={props.statusCode} />;
 };
 
-CustomErrorComponent.getInitialProps = async (contextData) => {
+CustomErrorComponent.getInitialProps = async (contextData: NextPageContext) => {
   // In case this is running in a serverless function, await this in order to give Sentry
   // time to send the error before the lambda exits
   await Sentry.captureUnderscoreErrorException(contextData);
