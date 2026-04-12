5cf279bacaeb9895a4e6bfc10be95122f3845b97
Xixiaoyang0 <150581968+Xixiaoyang0@users.noreply.github.com>
2026-04-12 09:49:38 +0800

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -21,8 +21,4 @@ export function proxy(request: NextRequest) {
   });
 }
 
-export const config = {
-  matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-  ],
-};
+;
