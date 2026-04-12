fd6c9090a85a5bd2be3412f08039e789686393af
Xixiaoyang0 <150581968+Xixiaoyang0@users.noreply.github.com>
2026-04-12 12:51:26 +0800

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -10,8 +10,20 @@ const config = {
       queue: "dummy",
     },
   },
+  // 刚才报错信息里提到的额外部分也给它补上
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
   build: {
-    // 依然保留这个核心配置，踢出 native 二进制依赖
+    // 依然保留咱们之前的“地雷排查”配置
     external: ["@swc/core", "@swc/wasm", "fsevents"],
     minify: true,
   },
