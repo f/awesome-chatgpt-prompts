a0971ce821119c3e52a19d7cd3b911fb4c5ed463
Xixiaoyang0 <150581968+Xixiaoyang0@users.noreply.github.com>
2026-04-12 17:34:35 +0800

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,8 +1,7 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 export default defineCloudflareConfig({
   // 目前保持默认即可，它会自动帮你配置好所有 Cloudflare 兼容性参数
-});
-
+}),
 // open-next.config.ts
 const config = {
   default: {
