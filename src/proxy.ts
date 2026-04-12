ac7669cc7c7460a19086fb1619403bcbcdade671
Xixiaoyang0 <150581968+Xixiaoyang0@users.noreply.github.com>
2026-04-12 10:37:20 +0800

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,4 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
+// middleware.ts
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
 export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
@@ -21,4 +25,23 @@ export function proxy(request: NextRequest) {
   });
 }
 
+
+export function middleware(request: NextRequest) {
+  return NextResponse.next();
+}
+
+// 显式添加边缘运行时配置
+export const config = {
+  runtime: 'edge', 
+  matcher: [
+    /*
+     * 匹配所有请求路径，但排除以下开头的内容：
+     * - api (API 路由)
+     * - _next/static (静态文件)
+     * - _next/image (图像优化文件)
+     * - favicon.ico (浏览器图标)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
 ;
